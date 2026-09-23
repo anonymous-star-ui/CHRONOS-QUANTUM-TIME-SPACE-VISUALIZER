a6587fb124f61788e7217b01f03843ce2290ea57
anonymous-star-ui <shashikiranpanuganti@gmail.com>
2026-09-23 08:28:26 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'chronos-v1';
+const CACHE_NAME = 'chronos-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -13,9 +13,18 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
